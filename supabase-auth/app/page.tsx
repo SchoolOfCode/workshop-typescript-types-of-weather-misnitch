cea99c69b72bd5fa4f56b84ad0e07667bf458ae0
Isuu1 <jakub.babelek@gmail.com>
2024-09-10 14:28:45 +0100

    Displayed data from database in Frontend

--- supabase-auth/app/page.tsx
+++ supabase-auth/app/page.tsx
@@ -2,10 +2,21 @@ import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
 export default async function Index() {
+  const supabase = createClient();
+
+  const { data: notes } = await supabase.from("notes").select();
+
+  console.log(notes);
+
   return (
     <>
+      {notes?.map((note) => {
+        return <h1>{note.title}</h1>;
+      })}
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
